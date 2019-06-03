fa39a48c644009fae90a8ba1694cfd2976adb48c
GitHub-Josue-Az <josue.alva@tecsup.edu.pe>
2019-06-03 18:21:59 -0500

    segundocommit

---
+++ Snapchat/SnapsViewController.swift
@@ -0,0 +1,22 @@
+
+import Foundation
+
+
+import UIKit
+
+
+class SnapsViewController: UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    @IBAction func cerrarSesionTapped(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+    
+    }
+    
+}
+
